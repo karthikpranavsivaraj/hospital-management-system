e8ee9d9143b8401fba31e8fb5b0c90911e8f331c
karthikpranavsivaraj <155215059+karthikpranavsivaraj@users.noreply.github.com>
2026-08-03 21:36:36 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://hospital-management-system-tgmj.onrender.com';
 
 // Auth Services
 export const registerDoctor = async (doctorData) => {
